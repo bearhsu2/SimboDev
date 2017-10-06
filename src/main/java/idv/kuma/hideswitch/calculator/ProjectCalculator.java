a16d37c58b0b95555a69ef7249bce8a160bf40dd
Yu-Song Syu <kuma@simbo.com.tw>
2017-10-07 00:31:53 +0800

    eliminate magic number

--- src/main/java/idv/kuma/hideswitch/calculator/ProjectCalculator.java
+++ src/main/java/idv/kuma/hideswitch/calculator/ProjectCalculator.java
@@ -3,12 +3,15 @@ package idv.kuma.hideswitch.calculator;
 import idv.kuma.hideswitch.Employee;
 
 public class ProjectCalculator extends BaseCalculator {
+
+    public static final double HUNDRED = 100D;
+
     public ProjectCalculator(Employee employee) {
         super(employee);
     }
 
     @Override
     public int calculate() {
-        return (int) (employee.getCommissionRate() / 100D * employee.getTurnover());
+        return (int) (employee.getCommissionRate() / HUNDRED * employee.getTurnover());
     }
 }
