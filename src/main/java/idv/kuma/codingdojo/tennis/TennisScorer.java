993a602ca9f7dfe8a0a01b6ae6d795091ac924f8
AngusWu <anguswu@simbo.com.tw>
2018-05-18 21:30:09 +0800

    CodingDojo

--- src/main/java/idv/kuma/codingdojo/tennis/TennisScorer.java
+++ src/main/java/idv/kuma/codingdojo/tennis/TennisScorer.java
@@ -1,4 +1,41 @@
 package idv.kuma.codingdojo.tennis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TennisScorer {
+    Map<String, Integer> players = new HashMap();
+    String winner = "";
+
+    public void addPlayer(String playerName) {
+        players.put(playerName, 0);
+    }
+
+    public int getPlayersScore(String playerName) {
+        return players.get(playerName);
+    }
+
+    public void addScore(String playerName) {
+        int playerScore = players.get(playerName);
+        if (playerScore < 30) {
+            players.put(playerName, playerScore + 15);
+        } else {
+            players.put(playerName, playerScore + 10);
+        }
+
+        if(players.get(playerName)>50){
+            winner = playerName;
+        }
+    }
+
+    public void checkDuce(){
+        int totalScore = 0;
+        for(int score: players.values()){
+            totalScore+=score;
+        }
+    }
+
+    public String checkWinner(){
+        return winner;
+    }
 }
--- src/test/java/idv/kuma/codingdojo/tennis/TennisScorerTest.java
+++ src/test/java/idv/kuma/codingdojo/tennis/TennisScorerTest.java
@@ -1,11 +1,15 @@
 package idv.kuma.codingdojo.tennis;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TennisScorerTest {
 
+    private String PLAYER_1 = "player1";
+    private String PLAYER_2 = "player2";
+    TennisScorer tennisScorer;
     @After
     public void tearDown() throws Exception {
         
@@ -13,11 +17,74 @@ public class TennisScorerTest {
 
     @Before
     public void setUp() throws Exception {
-        
+        tennisScorer = new TennisScorer();
+        tennisScorer.addPlayer(PLAYER_1);
+        tennisScorer.addPlayer(PLAYER_2);
     }
 
     @Test
     public void AtBeginning_AllZeros() throws Exception {
-        // your test here
+        Assert.assertEquals(tennisScorer.getPlayersScore(PLAYER_1),0 );
+        Assert.assertEquals(tennisScorer.getPlayersScore(PLAYER_2),0 );
+    }
+
+    @Test
+    public void TestPlayerScoreSuccess() throws Exception {
+        tennisScorer.addScore(PLAYER_1);
+        Assert.assertEquals(15, tennisScorer.getPlayersScore(PLAYER_1));
+    }
+
+    @Test
+    public void TestPlayerScoreThird() throws Exception {
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+        Assert.assertEquals(40, tennisScorer.getPlayersScore(PLAYER_1));
+    }
+    @Test
+    public void TestOnePlayerWin(){
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+        Assert.assertEquals(PLAYER_1, tennisScorer.checkWinner());
+    }
+
+    @Test
+    public void TestPlayersDeuce() throws Exception{
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_2);
+        tennisScorer.addScore(PLAYER_2);
+        tennisScorer.addScore(PLAYER_2);
+        Assert.assertEquals(0, tennisScorer.getPlayersScore(PLAYER_1)-tennisScorer.getPlayersScore(PLAYER_2));
+    }
+
+    @Test
+    public void TestPlayer1Advance() {
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_2);
+        tennisScorer.addScore(PLAYER_2);
+        tennisScorer.addScore(PLAYER_2);
+        tennisScorer.addScore(PLAYER_1);
+
+        Assert.assertEquals(50, tennisScorer.getPlayersScore(PLAYER_1));
+    }
+
+    @Test
+    public void TestPlayer1AdvanceThenWin() {
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_2);
+        tennisScorer.addScore(PLAYER_2);
+        tennisScorer.addScore(PLAYER_2);
+        tennisScorer.addScore(PLAYER_1);
+        tennisScorer.addScore(PLAYER_1);
+
+        Assert.assertEquals(PLAYER_1, tennisScorer.checkWinner());
     }
 }
