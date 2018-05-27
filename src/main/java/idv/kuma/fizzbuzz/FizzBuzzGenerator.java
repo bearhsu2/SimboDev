59f8c6d86e5a666535a860a91e33334a773d9daa
Jay Sheng <jay@simbo.com.tw>
2018-05-27 17:17:09 +0800

    refactor code

--- src/main/java/idv/kuma/fizzbuzz/FizzBuzzGenerator.java
+++ src/main/java/idv/kuma/fizzbuzz/FizzBuzzGenerator.java
@@ -3,12 +3,9 @@ package idv.kuma.fizzbuzz;
 public class FizzBuzzGenerator {
 
 
-    public String generate(int i) {
-        if (1 == i) {
-            return "1";
-        } else {
-            return "2";
-        }
+    public String generate(int number) {
+
+        return String.valueOf(number);
 
     }
 }
