f0a60341fae7b234528728b7b9c09f312ffdbe3c
Yu-Song Syu <kuma@simbo.com.tw>
2018-05-27 18:05:38 +0800

    final

--- src/main/java/idv/kuma/fizzbuzz/FizzBuzzGenerator.java
+++ src/main/java/idv/kuma/fizzbuzz/FizzBuzzGenerator.java
@@ -8,35 +8,21 @@ public class FizzBuzzGenerator {
 
         StringBuilder sb = new StringBuilder();
 
-        sb.append(tryAndPut(number, 3, "fizz"));
+        sb.append(tryDivisibleAndPut(number, 3, "fizz"));
 
-        sb.append(tryAndPut(number, 5, "buzz"));
-
+        sb.append(tryDivisibleAndPut(number, 5, "buzz"));
 
         return (0 == sb.length() ? sb.append(number) : sb).toString();
 
 
     }
 
-    private StringBuilder tryAndPut5(int number) {
-        return tryAndPut(number, 5, "buzz");
+    private StringBuilder tryDivisibleAndPut(int divident, int divisor, String keyword) {
+
+        return new StringBuilder().append(isDivisible(divident, divisor) ? keyword : "");
+
     }
 
-    private StringBuilder tryAndPut(int divident, int divisor, String keyword) {
-
-        if (shouldPut(divident, divisor)) {
-            return new StringBuilder().append(keyword);
-        } else {
-            return new StringBuilder();
-        }
-    }
-
-    private boolean shouldPut(int number, int i) {
-        if (isDivisible(number, i)) {
-            return true;
-        }
-        return false;
-    }
 
     private boolean isDivisible(int dividend, int divisor) {
         return 0 == dividend % divisor;
