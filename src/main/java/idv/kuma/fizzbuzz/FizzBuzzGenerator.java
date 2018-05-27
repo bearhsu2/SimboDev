d1564b635e5fc92dda3dd6d51425136dc15f64e0
Jay Sheng <jay@simbo.com.tw>
2018-05-27 17:37:05 +0800

    refactor code

--- src/main/java/idv/kuma/fizzbuzz/FizzBuzzGenerator.java
+++ src/main/java/idv/kuma/fizzbuzz/FizzBuzzGenerator.java
@@ -5,22 +5,21 @@ public class FizzBuzzGenerator {
 
     public String generate(int number) {
 
-        if (isDivisible(number, 15)) {
-            return "fizzbuzz";
-        }
+        if (shouldPut(number, 15)) return "fizzbuzz";
 
-        if (isDivisible(number, 3)) {
-            return "fizz";
-        }
+        if (shouldPut(number, 3)) return "fizz";
 
-        if (isDivisible(number, 5)) {
-            return "buzz";
-        }
-
-
+        if (shouldPut(number, 5)) return "buzz";
 
         return String.valueOf(number);
 
+    }
+
+    private boolean shouldPut(int number, int i) {
+        if (isDivisible(number, i)) {
+            return true;
+        }
+        return false;
     }
 
     private boolean isDivisible(int dividend, int divisor) {
