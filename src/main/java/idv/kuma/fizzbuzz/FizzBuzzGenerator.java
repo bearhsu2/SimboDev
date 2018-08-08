b3e5ea7cebbc6d5a8982ccc7a16a634ea992cc3f
Jay Sheng <jay@simbo.com.tw>
2018-08-08 14:59:20 +0800

    Refactor generator code

--- src/main/java/idv/kuma/fizzbuzz/FizzBuzzGenerator.java
+++ src/main/java/idv/kuma/fizzbuzz/FizzBuzzGenerator.java
@@ -5,6 +5,8 @@ public class FizzBuzzGenerator {
 
     public String generate(int i) {
 
+
+
         if (isFizz(i, 3)) {
             return "fizz";
         }
@@ -17,10 +19,14 @@ public class FizzBuzzGenerator {
     }
 
     private boolean isBuzz(int i, int i2) {
-        return 0 == i % 5;
+        return isDivided(i, 5);
+    }
+
+    private boolean isDivided(int i, int i2) {
+        return 0 == i % i2;
     }
 
     private boolean isFizz(int i, int i2) {
-        return  0 == i % 3;
+        return isDivided(i, 3);
     }
 }
