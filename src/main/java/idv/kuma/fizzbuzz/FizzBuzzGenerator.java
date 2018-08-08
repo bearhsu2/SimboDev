a2276cd797fd710f4b6f73c0b87ec5621da4d587
Jay Sheng <jay@simbo.com.tw>
2018-08-08 09:31:31 +0800

    initial of demo

--- src/main/java/idv/kuma/fizzbuzz/FizzBuzzGenerator.java
+++ src/main/java/idv/kuma/fizzbuzz/FizzBuzzGenerator.java
@@ -2,21 +2,4 @@ package idv.kuma.fizzbuzz;
 
 public class FizzBuzzGenerator {
 
-
-    public String generate(int number) {
-
-
-        if (number % 15 == 0){
-            return "fizzbuzz";
-        } else if (number % 3 == 0){
-            return "fizz";
-        } else if (number % 5 == 0){
-            return "buzz";
-        }
-
-        return "" + number;
-
-    }
-
-
 }
