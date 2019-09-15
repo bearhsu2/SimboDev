dca03bb152e523f3782f2a5d81258a891b4df039
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-15 13:17:09 +0800

    refactor: extract function aScore(times)

--- SimboDev24/sample/javascript/Tennis.test.js
+++ SimboDev24/sample/javascript/Tennis.test.js
@@ -12,13 +12,18 @@ test('When Initial Then Love All', () => {
 
 
 test('When a Then Fifteen Love', () => {
-    tennis.a();
+    aScore(1);
     checkScore('Fifteen Love')
 });
 
+function aScore(times) {
+    for (let i = 0; i < times; i++) {
+        tennis.a();
+    }
+}
+
 test('When aa Then Thirty Love', () => {
-    tennis.a();
-    tennis.a();
+    aScore(2);
     checkScore('Thirty Love')
 });
 
