45f91b49252dc2a1e3266c92ec33a3d9b3cbfd4c
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-18 23:15:59 +0800

    refactor: put love into lookup

--- SimboDev24/sample/javascript/Tennis.js
+++ SimboDev24/sample/javascript/Tennis.js
@@ -1,5 +1,6 @@
 const hitsToScore = new Map();
 
+hitsToScore.set(0, "Love");
 hitsToScore.set(1, "Fifteen");
 hitsToScore.set(2, "Thirty");
 hitsToScore.set(3, "Forty");
@@ -19,11 +20,8 @@ function Tennis() {
 
     this.score = function () {
 
-        if (this.aHits > 0) {
-            return hitsToScore.get(this.aHits) + ' Love';
-        } else if (this.bHits > 0) {
-            return 'Love ' + hitsToScore.get(this.bHits);
-
+        if (this.aHits > 0 || this.bHits > 0) {
+            return hitsToScore.get(this.aHits) + ' ' + hitsToScore.get(this.bHits);
         }
 
         return 'Love All';
