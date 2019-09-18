7116e4c8bb8e6a6c2ece655dd0e87b6eb930a891
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-18 22:59:51 +0800

    1:0

--- SimboDev24/sample/javascript/Tennis.js
+++ SimboDev24/sample/javascript/Tennis.js
@@ -1,12 +1,23 @@
 function Tennis() {
 
 
+    this.aHits = 0;
+
+    this.a = function () {
+        this.aHits++;
+    };
+
     this.score = function () {
+
+        if (this.aHits > 0) {
+            return 'Fifteen Love';
+        }
+
+
         return 'Love All';
     }
 
 }
-
 
 
 module.exports = Tennis;
--- SimboDev24/sample/javascript/Tennis.test.js
+++ SimboDev24/sample/javascript/Tennis.test.js
@@ -5,3 +5,9 @@ test('When Initial Then Love All', () => {
     const tennis = new Tennis();
     expect(tennis.score()).toBe('Love All');
 });
+
+test('When a Then Fifteen Love', () => {
+    const tennis = new Tennis();
+    tennis.a();
+    expect(tennis.score()).toBe('Fifteen Love');
+});
