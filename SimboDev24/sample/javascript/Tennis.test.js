4065ef71dcce65aef80ea6497210b3f631564f97
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-18 23:02:08 +0800

    refactor: make tennis a field

--- SimboDev24/sample/javascript/Tennis.test.js
+++ SimboDev24/sample/javascript/Tennis.test.js
@@ -1,13 +1,17 @@
 const Tennis = require('./Tennis');
 
 
+let tennis;
+
+beforeEach(() => {
+    tennis = new Tennis();
+});
+
 test('When Initial Then Love All', () => {
-    const tennis = new Tennis();
     expect(tennis.score()).toBe('Love All');
 });
 
 test('When a Then Fifteen Love', () => {
-    const tennis = new Tennis();
     tennis.a();
     expect(tennis.score()).toBe('Fifteen Love');
 });
