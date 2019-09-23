34a87158fc363a570a5222cf744e16cc62ca33ac
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-23 22:06:42 +0800

    back to init

--- SimboDev24/sample/java/simbodev/src/main/java/Tennis.java
+++ SimboDev24/sample/java/simbodev/src/main/java/Tennis.java
@@ -1,34 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Tennis {
-    private static Map<Integer, String> scoreLookup = new HashMap<>();
 
-    static {
-        scoreLookup.put(0, "Love");
-        scoreLookup.put(1, "Fifteen");
-        scoreLookup.put(2, "Thirty");
-        scoreLookup.put(3, "Forty");
-    }
-
-    private int aScore = 0;
-    private int bScore = 0;
-
-    public String score() {
-        if (aScore != bScore) {
-            return scoreLookup.get(aScore) + " " + scoreLookup.get(bScore);
-        }
-        if (aScore > 0) {
-            return "Fifteen All";
-        }
-        return "Love All";
-    }
-
-    public void firstPlayerHit() {
-        this.aScore++;
-    }
-
-    public void bHit() {
-        this.bScore++;
-    }
 }
