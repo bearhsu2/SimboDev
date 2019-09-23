b1d81309408b1b5c96ca8031cb0b806aefbc5e00
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-23 22:27:03 +0800

    refactor test: extract tennis to field and add @Before to test

--- SimboDev24/sample/java/simbodev/src/test/java/idv/kuma/TennisTest.java
+++ SimboDev24/sample/java/simbodev/src/test/java/idv/kuma/TennisTest.java
@@ -2,20 +2,27 @@ package idv.kuma;
 
 import kuma.Tennis;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TennisTest {
 
+    private Tennis tennis;
+
+    @Before
+    public void setUp() throws Exception {
+        tennis = new Tennis();
+
+    }
+
     @Test
     public void When_Init_Then_Love_All() {
-        Tennis tennis = new Tennis();
         Assert.assertEquals("Love All", tennis.score());
     }
 
 
     @Test
     public void When_a_Then_Fifteen_Love() {
-        Tennis tennis = new Tennis();
         tennis.a();
         Assert.assertEquals("Fifteen Love", tennis.score());
     }
