bdd4af946686b2e1910413303fd8c32d7b499824
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-23 22:25:24 +0800

    1:0

--- SimboDev24/sample/java/simbodev/src/main/java/kuma/Tennis.java
+++ SimboDev24/sample/java/simbodev/src/main/java/kuma/Tennis.java
@@ -2,4 +2,16 @@ package kuma;
 
 public class Tennis {
 
+    int aScore;
+
+    public String score() {
+        if (aScore > 0) {
+            return "Fifteen Love";
+        }
+        return "Love All";
+    }
+
+    public void a() {
+        aScore++;
+    }
 }
--- SimboDev24/sample/java/simbodev/src/test/java/idv/kuma/TennisTest.java
+++ SimboDev24/sample/java/simbodev/src/test/java/idv/kuma/TennisTest.java
@@ -1,5 +1,22 @@
 package idv.kuma;
 
+import kuma.Tennis;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TennisTest {
 
+    @Test
+    public void When_Init_Then_Love_All() {
+        Tennis tennis = new Tennis();
+        Assert.assertEquals("Love All", tennis.score());
+    }
+
+
+    @Test
+    public void When_a_Then_Fifteen_Love() {
+        Tennis tennis = new Tennis();
+        tennis.a();
+        Assert.assertEquals("Fifteen Love", tennis.score());
+    }
 }
