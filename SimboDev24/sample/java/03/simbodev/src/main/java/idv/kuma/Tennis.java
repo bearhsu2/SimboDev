6143ecfc3521e84c0605dfb6b5303e40a289e96c
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-24 14:58:55 +0800

    init 03

---
+++ SimboDev24/sample/java/03/simbodev/src/main/java/idv/kuma/Tennis.java
@@ -0,0 +1,11 @@
+package idv.kuma;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Hello world!
+ */
+public class Tennis {
+
+}
---
+++ SimboDev24/sample/java/03/simbodev/src/test/java/idv/kuma/TennisTest.java
@@ -0,0 +1,10 @@
+package idv.kuma;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TennisTest {
+}
