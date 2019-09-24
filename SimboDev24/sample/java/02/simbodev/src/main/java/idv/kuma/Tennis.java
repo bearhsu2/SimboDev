666af374c6c4caa31ca5fb4f46f53756ac0224ec
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-24 09:48:04 +0800

    init

---
+++ SimboDev24/sample/java/02/simbodev/src/main/java/idv/kuma/Tennis.java
@@ -0,0 +1,13 @@
+package idv.kuma;
+
+/**
+ * Hello world!
+ *
+ */
+public class Tennis
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ SimboDev24/sample/java/02/simbodev/src/test/java/idv/kuma/TennisTest.java
@@ -0,0 +1,20 @@
+package idv.kuma;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TennisTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
