358bfa04427b3f20445e07fa3e48a5e66e13ffd9
Yu-Song Syu <kuma@simbo.com.tw>
2019-09-25 23:16:59 +0800

    ><

--- SimboDev24/sample/java/04/simbodev/src/main/java/idv/kuma/Tennis.java
+++ SimboDev24/sample/java/04/simbodev/src/main/java/idv/kuma/Tennis.java
@@ -1,7 +1,87 @@
 package idv.kuma;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Hello world!
  */
 public class Tennis {
+
+    private final static Map<Integer, String> scoreToShow = new HashMap<>();
+
+    static {
+        scoreToShow.put(0, "Love");
+        scoreToShow.put(1, "Fifteen");
+        scoreToShow.put(2, "Thirty");
+        scoreToShow.put(3, "Forty");
+    }
+
+    private int aScore;
+    private int bScore;
+    private String aName;
+    private String bName;
+
+
+    public Tennis(String aName, String bName) {
+        this.aName = aName;
+        this.bName = bName;
+    }
+
+
+    public String score() {
+
+        if (sameScore()) {
+            return afterDeuce()
+                    ? "Deuce"
+                    : scoreToShow.get(aScore) + " All";
+        }
+
+        return afterDeuce()
+                ? leadingPlayer() + getPostFix()
+                : scoreToShow.get(aScore) + " " + scoreToShow.get(bScore);
+
+
+    }
+
+
+    private boolean sameScore() {
+        return aScore == bScore;
+    }
+
+
+    private boolean afterDeuce() {
+        return aScore >= 3 && bScore >= 3;
+    }
+
+
+    private String leadingPlayer() {
+        return aScore > bScore
+                ? aName
+                : bName;
+    }
+
+
+    private String getPostFix() {
+        return diffIsOne()
+                ? " Adv"
+                : " Wins";
+    }
+
+
+    private boolean diffIsOne() {
+        return Math.abs(aScore - bScore) == 1;
+    }
+
+
+    public void a() {
+
+        aScore++;
+
+    }
+
+
+    public void b() {
+        bScore++;
+    }
 }
--- SimboDev24/sample/java/04/simbodev/src/test/java/idv/kuma/TennisTest.java
+++ SimboDev24/sample/java/04/simbodev/src/test/java/idv/kuma/TennisTest.java
@@ -1,7 +1,132 @@
 package idv.kuma;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Unit test for simple App.
  */
 public class TennisTest {
+
+
+    private final Tennis tennis;
+
+
+    public TennisTest() {
+        tennis = new Tennis("Kuma", "Tom");
+    }
+
+
+    @Test
+    public void When_3_5_Tom_Wins() {
+        makeDeuce();
+        bScores(2);
+        runAndCheck("Tom Wins");
+    }
+
+
+    private void makeDeuce() {
+        aScores(3);
+        bScores(3);
+    }
+
+
+    private void bScores(int times) {
+        for (int i = 0; i < times; i++) {
+
+            tennis.b();
+        }
+    }
+
+
+    private void runAndCheck(String expect) {
+        Assert.assertEquals(expect, tennis.score());
+    }
+
+
+    private void aScores(int times) {
+        for (int i = 0; i < times; i++) {
+            tennis.a();
+        }
+    }
+
+
+    @Test
+    public void When_3_4_Tom_Adv() {
+        makeDeuce();
+        bScores(1);
+        runAndCheck("Tom Adv");
+    }
+
+
+    @Test
+    public void When_4_3_Kuma_Adv() {
+        makeDeuce();
+        aScores(1);
+        runAndCheck("Kuma Adv");
+    }
+
+
+    @Test
+    public void When_3_3_Then_Deuce() {
+        makeDeuce();
+        runAndCheck("Deuce");
+    }
+
+
+    @Test
+    public void When_2_2_Then_Thirty_All() {
+        aScores(2);
+        bScores(2);
+        runAndCheck("Thirty All");
+    }
+
+
+    @Test
+    public void When_1_1_Then_Fifteen_All() {
+        aScores(1);
+        bScores(1);
+        runAndCheck("Fifteen All");
+    }
+
+
+    @Test
+    public void When_0_2_Then_Love_Thirty() {
+        bScores(2);
+        runAndCheck("Love Thirty");
+    }
+
+
+    @Test
+    public void When_0_1_Then_Love_Fifteen() {
+        bScores(1);
+        runAndCheck("Love Fifteen");
+    }
+
+
+    @Test
+    public void When_3_0_Then_Forty_Love() {
+        aScores(3);
+        runAndCheck("Forty Love");
+    }
+
+
+    @Test
+    public void When_2_0_Then_Thirty_Love() {
+        aScores(2);
+        runAndCheck("Thirty Love");
+    }
+
+
+    @Test
+    public void When_1_0_Then_Fifteen_Love() {
+        aScores(1);
+        runAndCheck("Fifteen Love");
+    }
+
+
+    @Test
+    public void When_0_0_Then_Love_All() {
+        runAndCheck("Love All");
+    }
 }
